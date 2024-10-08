0a2e221475f46da704538b8e86e0ab3b82569fed
BT_2S14_32 <email@email>
2024-10-08 10:59:52 -0400

    Added the Linear Equation class!
    Added EquationRunner.java & continued the project!
    
    Fixed my EquationRunner.java code & tried to do the challenge!
    
    Edited all rounding issues!

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -6,25 +6,26 @@ public class EquationRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("0.00");
+        DecimalFormat dfCoord = new DecimalFormat("0");
 
-        System.out.println("Enter your first coordinate pair as (x1,y1): ");
+        System.out.println("Enter your first coordinate pair: ");
         String firstCoord = s.nextLine();
-        double x = Double.parseDouble(firstCoord.substring(1, firstCoord.indexOf(", ")));
-        double y = Double.parseDouble(firstCoord.substring(firstCoord.indexOf(", ") + 2, firstCoord.length()-1));
+        double x1 = Double.parseDouble(firstCoord.substring(1, firstCoord.indexOf(", ")));
+        double y1 = Double.parseDouble(firstCoord.substring(firstCoord.indexOf(", ") + 2, firstCoord.length()-1));
 
-        System.out.println("Enter your second coordinate pair as (x2,y2): ");
+        System.out.println("Enter your second coordinate pair: ");
         String secondCoord = s.nextLine();
         double x2 = Double.parseDouble(secondCoord.substring(1, secondCoord.indexOf(", ")));
         double y2 = Double.parseDouble(secondCoord.substring(secondCoord.indexOf(", ") + 2, secondCoord.length()-1));
 
-        LinearEquation equation = new LinearEquation(x, y, x2, y2);
+        LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
 
-        System.out.println(equation.getEquation());
-        System.out.println("First pair: (" + x + ", " + y + ")");
-        System.out.println("Second pair: (" + x2 + ", " + y2 + ")");
+        System.out.println("First pair: (" + dfCoord.format(x1) + ", " + dfCoord.format(y1) + ")");
+        System.out.println("Second pair: (" + dfCoord.format(x2) + ", " + dfCoord.format(y2) + ")");
         System.out.println("Slope of line: " + equation.getSlope());
         System.out.println("Y-intercept: " + equation.getYIntercept());
-        System.out.println("Distance between points: " + equation.getDistance());
+        System.out.println("Slope Intercept Form: " + equation.getEquation());
+        System.out.println("Distance between points: " + df.format(equation.getDistance()));
 
         System.out.println("Enter a third x-value: ");
         int x3 = s.nextInt();
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,37 +1,53 @@
 import java.text.DecimalFormat;
 
 public class LinearEquation {
-    private double x;
-    private double y;
+    private double x1;
+    private double y1;
     private double x2;
     private double y2;
-    private double slope, yIntercept;
-//    private DecimalFormat df = new DecimalFormat("0.00");
+    private int slopeNum;
+    private int slopeDen;
+    private double yIntercept;
+    private DecimalFormat df = new DecimalFormat("0.00");
 
-    public LinearEquation(double x, double y, double x2, double y2) {
-        this.x = x;
-        this.y = y;
+    public LinearEquation(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
-        this.slope = (y2 - y) / (x2 - x);
-        this.yIntercept = Math.round((y - (slope * x) * 100)) / 100;
+
+        // Calculates slope as a fraction
+        this.slopeNum = (int)(y2 - y1);
+        this.slopeDen = (int)(x2 - x1);
+        this.yIntercept = Math.round((y1 - (getSlope() * x1) * 100.0)) / 100.0;
     }
 
 
     public String getEquation() {
-        return "y = " + slope + "x + " + yIntercept;
+        String slopeString;
+
+        // Checks if the slope is an integer or a fraction
+        if (slopeNum % slopeDen == 0){
+            // If divisible, it's an integer
+            slopeString = String.valueOf(slopeNum / slopeDen);
+        }
+        else{
+            // If not divisible, keep it as s fraction
+            slopeString = slopeNum + "/" + slopeDen;
+        }
+        return "y = " + slopeString + "x + " + df.format(yIntercept);
     }
 
     public double getDistance() {
-        return Math.sqrt(Math.pow(x2 - x, 2) + Math.pow(y2 - y, 2));
+        return Math.round(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) * 100.0) / 100.0;
     }
 
-    public double calculateY(double x) {
-        return (slope * x) + yIntercept;
+    public double calculateY(double x1) {
+        return (getSlope() * x1) + yIntercept;
     }
 
     public double getSlope() {
-        return slope;
+        return (double) slopeNum / slopeDen;
     }
 
     public double getYIntercept() {
